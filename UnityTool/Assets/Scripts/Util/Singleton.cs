8b559a95040a26f9274ba68210899c1cd454947e
jljl73 <jljl73@naver.com>
2024-03-19 23:39:05 +0900

    싱글톤

---
+++ UnityTool/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mignon
+{
+    public class Singleton<T> where T : class, new()
+    {
+        static T instance;
+        public T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new T();
+                return instance;
+            }
+        }
+    }
+
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        static T instance;
+        public T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = GameObject.FindObjectOfType<T>();
+
+                if(instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    instance = singleton.AddComponent<T>();
+                }    
+
+                return instance;
+            }
+        }
+    }
+}
